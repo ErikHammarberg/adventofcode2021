b0d98ac32287b556940acff5693ba2e7a276125d
Erik Hammarberg Almén (mzq498) <erik.hammarberg-almen@teliacompany.com>
2021-12-02 08:05:07 +0100

    2 - 1 done

---
+++ src/com/xmas/DayTwo.java
@@ -0,0 +1,47 @@
+package com.xmas;
+
+public class DayTwo {
+
+    public int solveOne(String input) {
+        var inputs = Util.splitInput(input);
+
+        Sub solverSub = new Sub();
+        inputs.forEach(line -> {
+            var instr = line.split("\\s");
+            solverSub.takeInstruction(instr[0], Integer.parseInt(instr[1]));
+        });
+        return solverSub.getAnswer();
+    }
+
+    class Sub {
+        int horPosition;
+        int depth;
+
+        public void takeInstruction(String insctruction, int value) {
+            switch (insctruction) {
+                case
+                    "forward" -> forward(value);
+                case
+                    "down" -> down(value);
+                case
+                    "up" -> up(value);
+            }
+        }
+
+        public void forward(int value) {
+            horPosition+= value;
+        }
+        public void down(int value) {
+            depth +=value;
+        }
+
+        public void up(int value) {
+            depth -= value;
+        }
+
+        public int getAnswer() {
+            return horPosition * depth;
+        }
+    }
+
+}
